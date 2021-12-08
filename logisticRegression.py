c1972783c561ad6b5e1561fa938308c3052da455
fenceMeshwire <94779778+fenceMeshwire@users.noreply.github.com>
2021-12-08 23:29:25 +0100

    Update logisticRegression.py

--- logisticRegression.py
+++ logisticRegression.py
@@ -7,21 +7,21 @@ from sklearn.linear_model import LogisticRegression
 import numpy as np
 
 # Data (complaints, frustrated)
-X = np.array([[0, "No"],
+data = np.array([[0, "No"],
               [10, "No"],
               [15, "No"],
               [25, "Yes"],
               [35, "Yes"],
               [60, "Yes"]])
 
-n = len(X)
+n = len(data)
 
 # Building the model for the Logistic Regression:
-model = LogisticRegression().fit(X[::,0].reshape(n, 1), X[::,1])
+model = LogisticRegression().fit(data[::,0].reshape(n, 1), data[::,1])
 
 # Making a prediction:
 print(model.predict([[2],[7],[17],[36],[55]]))
 
 # Showing the transition from "No" to "Yes":
 for i in range(10, 30):
-    print("x=" + str(i) + " --> " + str(model.predict_proba([[i]])))
+    print("complaints=" + str(i) + " --> " + str(model.predict_proba([[i]])))
